0ded16a22ca857838796b24fe498ca0e5ee4d4dd
shivam.preet <preetshivam111@gmail.com>
2021-03-04 18:23:34 +0530

    myFirstCommit

---
+++ DotnetCoreApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DotnetCoreApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
